8c64c25776d7e02eaa66ca280642eabe9a1f838d
Wisnu Box <hwisnu222@gmail.com>
2025-11-09 12:33:11 +0700

    feat: ui downloader

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Container,
+  VStack,
+  HStack,
+  Heading,
+  Text,
+  Input,
+  Button,
+  Flex,
+  InputGroup,
+  Box,
+  Card,
+  Image,
+} from "@chakra-ui/react";
+import "./App.css";
+import Table from "./components/Table";
+import { Link2, Download, Search, Github } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box pb={8}>
+      <Flex
+        justify="space-between"
+        p={4}
+        alignItems="center"
+        position="sticky"
+        top={0}
+      >
+        <Heading>Fagram</Heading>
+        <Button colorScheme="red" borderRadius="full">
+          <Github />
+          Github
+        </Button>
+      </Flex>
+      <Container>
+        <VStack gap={4} w="100%" pt={20} mb={8}>
+          <Heading className="sora-600" size="5xl">
+            Instagram Downloader
+          </Heading>
+          <Text w="50%" textAlign="center">
+            We all know that feeling: seeing the perfect tutorial, a hilarious
+            Reel, or a stunning photo... and then losing it forever in your
+            saved list
+          </Text>
+          <InputGroup
+            startElement={<Link2 />}
+            endElement={
+              <Button
+                colorPalette="pink.600"
+                variant="solid"
+                borderRadius="full"
+              >
+                <Download />
+                Download
+              </Button>
+            }
+            pr={1}
+          >
+            <Input
+              placeholder="https://instagram.com/p/G453HGDH/"
+              size="xl"
+              type="url"
+              borderRadius="full"
+            />
+          </InputGroup>
+        </VStack>
+        <Flex gap={4} mb={8}>
+          <Card.Root maxW="sm" overflow="hidden">
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              mb={2}
+            />
+            <Card.Footer gap="2">
+              <Button variant="solid" w="100%" borderRadius="full">
+                <Download />
+                Download
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+        </Flex>
+
+        <Flex
+          w="100%"
+          gap={4}
+          direction="column"
+          justify="start"
+          alignItems="start"
+        >
+          <Heading>History</Heading>
+          <HStack justify="end">
+            <InputGroup startElement={<Search />}>
+              <Input placeholder="Search username, link..." />
+            </InputGroup>
+          </HStack>
+          <Table />
+        </Flex>
+      </Container>
+    </Box>
+  );
 }
 
-export default App
+export default App;
---
+++ web/src/components/Table.tsx
@@ -0,0 +1,69 @@
+import { Table as ChakraTable, IconButton, HStack } from "@chakra-ui/react";
+import { Delete, View } from "lucide-react";
+
+export default function Table() {
+  return (
+    <ChakraTable.ScrollArea
+      borderWidth="1px"
+      rounded="md"
+      minHeight="160px"
+      w="100%"
+      borderRadius="xl"
+    >
+      <ChakraTable.Root size="lg" stickyHeader>
+        <ChakraTable.Header>
+          <ChakraTable.Row bg="bg.subtle">
+            <ChakraTable.ColumnHeader>Username</ChakraTable.ColumnHeader>
+            <ChakraTable.ColumnHeader>Link</ChakraTable.ColumnHeader>
+            <ChakraTable.ColumnHeader>Status</ChakraTable.ColumnHeader>
+            <ChakraTable.ColumnHeader textAlign="end">
+              Action
+            </ChakraTable.ColumnHeader>
+          </ChakraTable.Row>
+        </ChakraTable.Header>
+
+        <ChakraTable.Body>
+          {items.map((item) => (
+            <ChakraTable.Row key={item.id}>
+              <ChakraTable.Cell>{item.username}</ChakraTable.Cell>
+              <ChakraTable.Cell>{item.link}</ChakraTable.Cell>
+              <ChakraTable.Cell>{item.status}</ChakraTable.Cell>
+              <ChakraTable.Cell textAlign="end">
+                <HStack gap={2} justify="end">
+                  <IconButton variant="outline" borderRadius="full">
+                    <Delete />
+                  </IconButton>
+
+                  <IconButton borderRadius="full">
+                    <View />
+                  </IconButton>
+                </HStack>
+              </ChakraTable.Cell>
+            </ChakraTable.Row>
+          ))}
+        </ChakraTable.Body>
+      </ChakraTable.Root>
+    </ChakraTable.ScrollArea>
+  );
+}
+
+const items = [
+  {
+    id: 1,
+    username: "Laptop",
+    link: "https://instagram.com/p/safsa323KJ",
+    status: "done",
+  },
+  {
+    id: 1,
+    username: "Laptop",
+    link: "https://instagram.com/p/safsa323KJ",
+    status: "done",
+  },
+  {
+    id: 1,
+    username: "Laptop",
+    link: "https://instagram.com/p/safsa323KJ",
+    status: "done",
+  },
+];
