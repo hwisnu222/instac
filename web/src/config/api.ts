e261d67bbefc64e5414da789af3b5ee139fa6710
Wisnu Box <hwisnu222@gmail.com>
2025-11-12 11:43:50 +0700

    feat: host axios for development

--- web/src/config/api.ts
+++ web/src/config/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.NODE_ENV === "production"
-    ? "/api"
-    : "http://localhost:8000/api";
+  import.meta.env.NODE_ENV === "development"
+    ? "http://localhost:8000/api"
+    : "/api";
 
 export const API_BASE = axios.create({
   baseURL: BASE_URL,
